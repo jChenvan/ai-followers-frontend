a6ba4aec795270d03463ce605d179071b8411440
paulBlartCallMop <188939308+paulBlartCallMop@users.noreply.github.com>
2025-02-13 10:59:06 -0500

    Add responsive design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,25 @@
 import Logo from '/unai-logo.png'
 import '../styles/Header.css'
+import { Link } from 'react-router-dom'
+import { useRef } from 'react'
 
 function Header() {
+  const menuRef = useRef(null);
+
   return (
     <header>
         <div className="logo">
-            <img src={Logo} alt="logo" width={300}/>
-            <div className='text'></div>
+            <div className="image"><img src={Logo} alt="logo" width={300}/></div>
+            <div className="menu">
+              <button onClick={()=>menuRef.current.classList.toggle('hidden')}>
+                <img src="/menu.svg" alt="" height={80}/>
+              </button>
+              <ul ref={menuRef} className='hidden'>
+                <li><Link to={'/feed'}>Feed</Link></li>
+                <li><Link to={'/messages'}>Chats</Link></li>
+                <li><Link to={'/friends'}>Friends</Link></li>
+              </ul>
+            </div>
         </div>
     </header>
   )
